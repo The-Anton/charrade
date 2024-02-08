37266abfba36c96c6de614aaf1f920a8217a073d
The-Anton <shesingh@redhat.com>
2024-02-08 23:54:23 +0530

    start page UI changes

--- charrade-client/src/pages/Start.jsx
+++ charrade-client/src/pages/Start.jsx
@@ -1,18 +1,22 @@
-import './Home.css';
+import './Start.css';
 import AvatarSelector from '../components/Avatar';
 
 function Start() {
     return (
         <>
-            <div>
-                <h1>Charrad</h1>
+            <div className='header'>
+                <div className='logo '><h1 className='font-face-rh'>Charrade</h1></div>
+
+                <div className='username-input'><input type="text" placeholder="Enter Your Name"></input></div>
             </div>
-            <input ></input>
-            <div>
-                <button>Play</button>
-                <button>Create Room</button>
+            <div className='control-content'>
+                <AvatarSelector></AvatarSelector>
+                <div className='start-controls'>
+                    <button>Play</button>
+                    <button>Create Room</button>
+                </div>
             </div>
-            <AvatarSelector></AvatarSelector>
+
         </>
     );
 }
